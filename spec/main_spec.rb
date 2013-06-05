ee7e6922e2a082fd3375b7749455026c386d126e
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-06-05 18:17:40 +0930

    Slight syntax refactoring

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,15 +1,14 @@
 describe "Application 'Magic8Ball'" do
   extend SpecHelper
 
-  before do
-    @app = Application.application_instance
-  end
+  before { @app = Application.application_instance }
 
   it "has one window" do
     @app.windows.size.should.equal(1)
   end
 
   it "has a root view controller" do
-    @app.key_window.root_view_controller.class.should.equal(Magic8BallController)
+    @app.key_window.root_view_controller.class.should
+      .equal(Magic8BallController)
   end
 end
