6852827fd5ea61fe25ecc5cf58752c1720e3e05a
Paul Fioravanti <paul.fioravanti@gmail.com>
2014-02-16 22:13:25 +1100

    Move window property initialisation into the window class

--- app/application/application_delegate.rb
+++ app/application/application_delegate.rb
@@ -4,7 +4,5 @@ class ApplicationDelegate
   def initialize
     # return true if RUBYMOTION_ENV == 'test'
     @window = ApplicationWindow.new
-    @window.root_view_controller = Magic8BallController.new
-    @window.make_key_and_visible
   end
 end
--- app/views/application/application_window.rb
+++ app/views/application/application_window.rb
@@ -3,5 +3,7 @@ class ApplicationWindow < UIWindow
 
   def initialize
     self.frame = DeviceScreen.main_screen.bounds
+    self.root_view_controller = Magic8BallController.new
+    self.make_key_and_visible
   end
 end
